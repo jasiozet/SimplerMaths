4af05e2118e29c84481e316a82461ea2befc1412
Jan Zalewski <18668118+jasiozet@users.noreply.github.com>
2022-07-10 15:46:57 +0200

    more property testing

--- test/SimplerMathTests/PropertyBasedTests.fs
+++ test/SimplerMathTests/PropertyBasedTests.fs
@@ -11,3 +11,18 @@ let ``Every Integer divided by Integer multiplied by same Integer remains itself
     let b = b' .Get
     a .div b * (float b) |> should (equalWithin 0.1) (float a)
   Check.QuickThrowOnFailure intDivIntMulSameIntRemainsItself
+
+[<Fact>]
+let ``Every Float divided by Integer multiplied by same Integer remains itself`` () =
+  let intDivIntMulSameIntRemainsItself (a:System.Int64) (b':NonZeroInt) =
+    let b = b' .Get
+    a .div b * (float b) |> should (equalWithin 0.1) (float a)
+  Check.QuickThrowOnFailure intDivIntMulSameIntRemainsItself
+
+[<Fact>]
+let ``Every Long divided by Integer multiplied by same Integer remains itself`` () =
+  let intDivIntMulSameIntRemainsItself (a':NormalFloat) (b':NonZeroInt) =
+    let a = b' .Get
+    let b = b' .Get
+    a .div b * (float b) |> should (equalWithin 0.1) a
+  Check.QuickThrowOnFailure intDivIntMulSameIntRemainsItself
